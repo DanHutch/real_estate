11131ce0276e6a433c11ac1b88db86ee20027ed2
DanHutch <danhutch5280@gmail.com>
2018-08-29 13:19:20 -0600

    iteration 2 initial commit

---
+++ lib/room.rb
@@ -0,0 +1,16 @@
+## room.rb
+
+require 'pry'
+
+class Room
+  attr_reader :category,
+              :area
+
+  def initialize(category, length, width)
+    @category = category
+    @area = length * width
+  end
+
+
+
+end
