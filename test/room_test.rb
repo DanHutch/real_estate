35cf581e9418cbe4c3514317bb6266c6376901ed
DanHutch <danhutch5280@gmail.com>
2018-08-29 13:16:35 -0600

    finished iteration 1

--- test/room_test.rb
+++ test/room_test.rb
@@ -7,6 +7,15 @@ require 'pry'
 
 class RoomTest < Minitest::Test
 
+  def test_it_exists
+    room = Room.new(:bedroom, 10, 13)
+    assert_instance_of(Room, room)
+  end
 
+  def test_it_has_attributes
+    room = Room.new(:bedroom, 10, 13)
+    assert_equal(:bedroom, room.category)
+    assert_equal(130, room.area)
+  end
 
 end
